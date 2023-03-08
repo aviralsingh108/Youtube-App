0793e393aad858598c5716634733bf26fcdda0ac
aviralsingh108 <admiralaviral@gmail.com>
2023-03-08 23:58:18 +0530

    dynamic video loading on watch page

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -10,7 +10,19 @@ const WatchPage = () => {
   useEffect(() => {
     dispatch(closeMenu());
   }, []);
-  return <div>WatchPage</div>;
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 };
 
 export default WatchPage;
