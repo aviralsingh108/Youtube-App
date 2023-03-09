bcac5446641d6f0396a42553fca95a8a73f78d63
aviralsingh108 <admiralaviral@gmail.com>
2023-03-09 23:48:52 +0530

    using recursion for n-level nested comments

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -55,7 +55,7 @@ const commentsData = [
 const Comment = ({ data }) => {
   const { name, text, replies } = data;
   return (
-    <div className="flex shadow-lg p-2 rounded-lg border border-gray-200">
+    <div className="flex shadow-lg p-2 rounded-lg border border-gray-200 my-2">
       <img
         className="w-12 h-12"
         alt="user-icon"
@@ -68,14 +68,16 @@ const Comment = ({ data }) => {
     </div>
   );
 };
+
 const CommentsList = ({ comments }) => {
-  return (
+  return comments.map((comment, index) => (
     <div>
-      {comments.map((comment, index) => (
-        <Comment key={index} data={comment} />
-      ))}
+      <Comment key={index} data={comment} />
+      <div className="pl-5 border border-l-black ml-5">
+        <CommentsList comments={comment.replies} />
+      </div>
     </div>
-  );
+  ));
 };
 const CommentsContainer = () => {
   return (
