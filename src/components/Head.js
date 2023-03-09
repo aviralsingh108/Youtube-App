4134dbe2f171fa55461ef3509597ce8a621f61ab
aviralsingh108 <admiralaviral@gmail.com>
2023-03-09 17:35:33 +0530

    search list auto suggestion added with debouncing of 200ms

--- src/components/Head.js
+++ src/components/Head.js
@@ -6,7 +6,7 @@ import { YOUTUBE_SEARCH_API } from "../utils/Constants";
 
 const Header = () => {
   const [searchQuery, setSearchQuery] = useState("");
-
+  const [suggestions, setSuggestions] = useState([]);
   useEffect(() => {
     const timer = setTimeout(() => {
       getSearchSuggestions();
@@ -22,6 +22,7 @@ const Header = () => {
     const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await data.json();
     console.log(json);
+    setSuggestions(json[1]);
   };
 
   const dispatch = useDispatch();
@@ -47,17 +48,29 @@ const Header = () => {
         />
         {/* </Link> */}
       </div>
-      <div className="col-span-10 mx-40 -my-2">
-        <input
-          className="w-1/2  p-2 border border-gray-400 ml-5 rounded-l-full"
-          type="text"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button className=" px-5 py-2 border border-gray-400 rounded-r-full">
-          🔍
-        </button>
+      <div className="col-span-10 ml-40 -my-2">
+        <div>
+          <input
+            className="w-1/2 px-7 py-2 border border-gray-400 ml-5 rounded-l-full"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <button className=" px-5 py-2 border border-gray-400 rounded-r-full">
+            🔍
+          </button>
+        </div>
+        <div className="fixed bg-white my-1 py-2 px-4 ml-6 rounded-lg w-[29.8rem] shadow-lg border border-gray-100">
+          <ul>
+            {suggestions.map((suggestion) => (
+              <li key={suggestion} className="hover:bg-gray-100">
+                🔍 {suggestion}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
+
       <div className="flex col-span-1 -my-2">
         <img
           className="h-9"
