201ef5d221521ff56955bb99ff700a18e920e4ab
aviralsingh108 <admiralaviral@gmail.com>
2023-03-11 02:26:00 +0530

    random name and messages for chat to display in reverse order

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
+import { generate, randomText } from "../utils/helper";
 import store from "../utils/store";
 import ChatMessage from "./ChatMessage";
 
@@ -9,12 +10,12 @@ const LiveChat = () => {
   const chatMessages = useSelector((store) => store.chat.messages);
   useEffect(() => {
     const i = setInterval(() => {
-      dispatch(addMessage({ name: "Aviral", message: "Adding some message" }));
+      dispatch(addMessage({ name: generate(), message: randomText(25) }));
     }, 2000);
     return () => clearTimeout(i);
   }, []);
   return (
-    <div className="ml-2 p-2 border border-black w-full h-[600px] bg-slate-100 rounded-lg overflow-y-scroll">
+    <div className="ml-2 p-2 border border-black w-full h-[600px] bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
       {chatMessages.map((c, i) => (
         <ChatMessage key={i} name={c.name} message={c.message} />
       ))}
