adc1a87b57fef36e250916805715450246cc1265
aviralsingh108 <admiralaviral@gmail.com>
2023-03-10 23:49:48 +0530

    added redux chatSlice

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ChatMessage from "./ChatMessage";
 
 const LiveChat = () => {
+  useEffect(() => {
+    const i = setTimeout(() => {}, 2000);
+    return () => clearTimeout(i);
+  }, []);
   return (
     <div className="ml-2 p-2 border border-black w-full h-[600px] bg-slate-100 rounded-lg">
       <ChatMessage name={"Aviral"} message={"This is my first comment"} />
