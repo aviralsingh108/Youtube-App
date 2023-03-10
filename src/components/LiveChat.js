c55c66b7e77ded240e2a7bff2c85c8f051069453
aviralsingh108 <admiralaviral@gmail.com>
2023-03-10 23:59:03 +0530

    reading messages from redux and scrollable live chat messages

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
+import store from "../utils/store";
 import ChatMessage from "./ChatMessage";
 
 const LiveChat = () => {
   const dispatch = useDispatch();
+  const chatMessages = useSelector((store) => store.chat.messages);
   useEffect(() => {
     const i = setInterval(() => {
       dispatch(addMessage({ name: "Aviral", message: "Adding some message" }));
@@ -12,11 +14,10 @@ const LiveChat = () => {
     return () => clearTimeout(i);
   }, []);
   return (
-    <div className="ml-2 p-2 border border-black w-full h-[600px] bg-slate-100 rounded-lg">
-      <ChatMessage name={"Aviral"} message={"This is my first comment"} />
-      <ChatMessage name={"Aviral"} message={"This is my first comment"} />
-      <ChatMessage name={"Aviral"} message={"This is my first comment"} />
-      <ChatMessage name={"Aviral"} message={"This is my first comment"} />
+    <div className="ml-2 p-2 border border-black w-full h-[600px] bg-slate-100 rounded-lg overflow-y-scroll">
+      {chatMessages.map((c, i) => (
+        <ChatMessage key={i} name={c.name} message={c.message} />
+      ))}
     </div>
   );
 };
